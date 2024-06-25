cdbcf966b66929cf7ed8b950be5ba8a91ee3d258
Quokka <wotjr1142@gmail.com>
2024-06-25 16:04:08 +0900

    0625

---
+++ VueTest/09_VueBlogWithJson/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import TheWelcom from "@/components/home/TheWelcom.vue";
+</script>
+
+<template>
+  <div class="home">
+    <main>
+      <TheWelcom />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70%;
+  margin-top: 1.5%;
+  margin-left: auto;
+  margin-right: auto;
+  height: 90vh;
+  background-color: white;
+  padding: 13px;
+  border: 1px solid black;
+}
+.home {
+  background-color: black(255, 135, 135, 0.452);
+}
+</style>
